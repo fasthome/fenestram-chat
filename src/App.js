35c312c2310c2705766f32ddf55f2bf051e0a5f3
mestoevinal <mestoevinal@mail.ru>
2022-07-13 14:46:37 +0300

    create registration pages and stylization for mobile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import styled from 'styled-components'
+import Inputnumber from "./components/registration/Inputnumber";
+import Falsecod from "./components/registration/Falsecod";
+import Inputcod from "./components/registration/Inputcod";
+import ReturnInputCod from "./components/registration/returnInputCod";
+import InputData from "./components/registration/inputData";
+import 'antd/dist/antd.min.css'
+
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  font-family: Geometria;
+`
+
 
 function App() {
   return (
     <div>
-          <p>SLS</p>
+          <Wrapper>
+              <Inputnumber />
+              <Falsecod />
+              <Inputcod />
+              <ReturnInputCod />
+              <InputData />
+          </Wrapper>
     </div>
   );
 }
